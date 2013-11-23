565ccfdcd4b8938b89b865727970973a669abc58
be9em0t <vla@chaosgroup.com>
2013-11-23 21:37:56 +0200

    Initial commit - Walk-run blendtree

---
+++ Assets/Scripts/Mecanim03.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mecanim03 : MonoBehaviour {
+
+	// public float animSpeed = 1.5f;				// a public setting for overall animator animation speed
+	public float DampTime = 25f;
+	private Animator anim;							// a reference to the animator on the character
+	private AnimatorStateInfo currentBaseState;			// a reference to the current state of the animator, used for base layer
+	static int idleState = Animator.StringToHash("Base Layer.Stand_Idle");	
+	//static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+	static int walkState = Animator.StringToHash("Base Layer.Walk");
+	static int runState = Animator.StringToHash("Base Layer.Run");
+	
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		float v = Input.GetAxis("Vertical");				// setup v variables as our vertical input axis
+		anim.SetFloat("Speed", v);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		anim.SetFloat("Direction", h, DampTime, Time.deltaTime); 						// set our animator's float parameter 'Direction' equal to the horizontal input axis		
+		currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// set our currentState variable to the current state of the Base Layer (0) of animation
+
+		if (Input.GetKey(KeyCode.LeftShift))		//While LeftShift pressed
+		{
+			//anim.SetBool("Sprint", true);					//Turn on boolean
+			anim.SetFloat("Walk-Run", 1f, DampTime, Time.deltaTime);
+		}
+		else
+		{
+			anim.SetFloat("Walk-Run", 0f, DampTime, Time.deltaTime);					
+		}
+
+
+
+		if (currentBaseState.nameHash == idleState)
+		{
+				Debug.Log("idle");
+
+		}
+
+		if (currentBaseState.nameHash == walkState)
+		{
+				Debug.Log("walk");
+			
+		}
+
+		if (currentBaseState.nameHash == runState)
+		{
+				Debug.Log("run");
+			
+		}
+
+	}
+}
---
+++ Assets/Scripts/MecanimBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class MecanimBehaviour : MonoBehaviour {
+	
+	//private float Speed = 1f;
+	//private float Direction = 1f;
+	private Animator anim;							// a reference to the animator on the character
+	public float DirDampTime = .25f;				//smooth turns	
+	
+
+	// Use this for initialization
+	void Start () {
+		// initialising reference variables
+ 		anim = GetComponent<Animator>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		anim.SetBool ("Jump", Input.GetKey(KeyCode.J));
+		//Debug.Log ("Jump state");
+		
+		float v = Input.GetAxis("Vertical");					// setup v variables as our vertical input axis
+		float h = Input.GetAxis("Horizontal");					// setup h variable as our horizontal input axis
+		//anim.SetFloat("Speed", v);						// animator's 'Speed' equal to the vertical input axis				
+		//anim.SetFloat("Direction", h); 					// animator's 'Direction' equal to the horizontal input axis	
+
+		anim.SetFloat("Speed", h*h+v*v);						// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		anim.SetFloat("Direction", h, DirDampTime, Time.deltaTime); // set our animator's float parameter 'Direction' equal to the horizontal input axis	
+
+	}
+	
+		void FixedUpdate ()
+	{
+	}
+}
